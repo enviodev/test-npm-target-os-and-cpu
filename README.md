d16a8acc291489262f7102a8222ad1fde627b33a
Jason <jason@envio.dev>
2024-05-08 09:49:37 +0200

    Cleanup and readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This script allows us to determine which OS and CPU target is being used by the current environment according to npm and node.
 
+To use the script run:
+```bash
+pnpm i
+pnpm test
+```
+
+(note npm or yarn can also be used, but envio only supports pnpm officially, so rather use that.)
+
+
+#### Dev notes (not for users)
+
+Build and publish the test packages run: `node ./packages/index.js`.
--- src/index.js
+++ src/index.js
@@ -21,7 +21,24 @@ function checkDependencies() {
   ];
 
   const targetDependencies = [
-
+    "test-cpu-package-arm",
+    "test-cpu-package-arm64",
+    "test-cpu-package-ia32",
+    "test-cpu-package-mips",
+    "test-cpu-package-mipsel",
+    "test-cpu-package-ppc",
+    "test-cpu-package-ppc64",
+    "test-cpu-package-s390",
+    "test-cpu-package-s390x",
+    "test-cpu-package-x64",
+    "test-os-package-aix",
+    "test-os-package-android",
+    "test-os-package-darwin",
+    "test-os-package-freebsd",
+    "test-os-package-linux",
+    "test-os-package-openbsd",
+    "test-os-package-sunos",
+    "test-os-package-win32",
   ];
 
   console.log('\nChecking for dependency directories in node_modules:');
@@ -35,8 +52,8 @@ function checkDependencies() {
   });
 
   console.log('\nChecking for dependencies that only target specific OS or CPU architectures:');
-  dependencies.forEach(dep => {
-    const depPath = path.join(__dirname, '../node_modules/', dep);
+  targetDependencies.forEach(dep => {
+    const depPath = path.join(__dirname, '../node_modules/@envio-dev', dep);
     if (fs.existsSync(depPath)) {
       console.log(`Found: ${dep}`);
     } else {
